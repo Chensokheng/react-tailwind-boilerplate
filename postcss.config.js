34b054118f0b14da059a60b54b70c4979b951312
Chensokheng <chensokheng6@gmail.com>
2020-12-08 10:36:14 +0700

    :tada:

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+const tailwindcss = require('tailwindcss');
+
+module.exports = {
+  plugins: [
+    tailwindcss('./tailwind.config.js'),
+    require('autoprefixer'),
+    process.env.NODE_ENV === 'production'
+      ? require('@fullhuman/postcss-purgecss')({
+          content: ['./src/**/*.js', './public/index.html'],
+          defaultExtractor: (content) =>
+            content.match(/[A-Za-z0-9-_:/]+/g) || [],
+        })
+      : '',
+  ],
+};
